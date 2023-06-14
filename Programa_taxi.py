9c7732436636f7c609e1b8232de7e1e71196e73b
Miguel <miguel.almespinoza@gmail.com>
2023-06-14 13:26:43 +0200

    Contrucción del esqueleto del programa

--- Programa_taxi.py
+++ Programa_taxi.py
@@ -1,23 +1,24 @@
 #! /usr/bin/env python 3
+import time
 
-#TODO  El programa da la bienvenida, explica el funcionamiento y se queda a la espera de que se le indique por input que la carrera ha comenzado.
+class Taxi:
+    def __init__(self):
+        self.en_movimiento = False
+        self.tiempo_inicio = None
+        self.tarifa_parado = 2
+        self.tarifa_movimiento = 5
+        self.total = 0
+    def empezar_carrera(self):
+    
+    def pausar_carrera(self):
+    
+    def calcular_total(self):
+
+def main():
+
 def mostrar_bienvenida():
     print("Bienvenido a DigiTaxi")
     print("El programa para calcular las tarifas de tus carreras")
 
-#TODO Cuando se le dé la orden de empezar, el programa debe empezar a calcular la tarifa contando con que el taxi empezará siempre parado.
 
-"""
-Mientras el taxi esté parado, al cliente se le cobrarán 2 céntimos por segundo de carrera. 
-En el momento en el que el taxi arranque, mientras esté en movimiento, se cobrarán 5 céntimos por segundo. 
-Cuando se le indique al programa que la carrera ha terminado, el programa calculará el total (en Euros).
 
-"""
-
-#TODO  Cuando se le indique que el taxi se pone en marcha, debe empezar a aplicar el precio del taxi en movimiento.
-
-#TODO Se le debe poder indicar al programa que el taxi se queda quieto y que vuelve a arrancar sin acabar la carrera (el taxi puede parar en un semáforo, en un atasco, en un stop, etc).
-
-#TODO  Al indicarle al programa que la carrera ha acabado, debe devolver el precio total en euros.
-
-#TODO  Al acabar la carrera y devolver el total, el programa debe quedarse a la espera de que se le indique que empieza una nueva carrera sin necesidad de cerrarlo y ejecutarlo de nuevo
