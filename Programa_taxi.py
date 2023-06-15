f543efa26a7cb2ce333eaa4a563db2190a22244b
Miguel <miguel.almespinoza@gmail.com>
2023-06-15 11:52:48 +0200

    Añadimos comentarios al código

--- Programa_taxi.py
+++ Programa_taxi.py
@@ -1,13 +1,17 @@
 #! /usr/bin/env python 3
 import time
 
+# Designamos la clase Taxi
 class Taxi:
+
+    # Definimos el constructor.
     def __init__(self):
         self.en_movimiento = False
         self.tiempo_inicio = None
         self.tarifa_parado = 2
-        self.tarifa_movimiento = 5
-        
+        self.tarifa_movimiento = 5  
+
+    # Definimos el método que arranca el taximetro.    
     def empezar_carrera(self):
         self.total = 0
         self.tiempo_inicio = time.time()
@@ -15,7 +19,7 @@ class Taxi:
         print("La carrera ha comenzado, ¡allá vamos!")
 
 
-    
+    # Definimos el método que calcula la tarifa acumulada cuando nos ponemos en movimiento. 
     def continuar_carrera(self):
         if not self.en_movimiento:
             tiempo_arrancar = time.time()
@@ -27,7 +31,7 @@ class Taxi:
             print(f"El taxi continua su recorrido. Total acumulado {euros:.2f} euros.")
 
 
-
+    # Definimos el método que calcula la tarifa acumulada cuando nos detenemos.
     def pausar_carrera(self):
         if self.en_movimiento:
             tiempo_pausa = time.time()
@@ -38,7 +42,7 @@ class Taxi:
             print(f"El taxi se ha detenido temporalmente. Total acumulado {euros:.2f} euros.")
 
     
-    
+    # Definimos el método que calcula el total de la tarifa al final de la carrera.
     def calcular_total(self):
         if self.en_movimiento:
             tiempo_pausa = time.time()
@@ -56,10 +60,13 @@ class Taxi:
         euros =self.total/ 100       
         print(f"La carrera ha terminado. Total a pagar {euros:.2f} euros.")
         
-
+# Definimos la función principal del programa que llamará a la clase y los métodos de taxi.
 def main():
     print("Bienvenido a DigiTaxi")
     print("El programa para calcular las tarifas de tus carreras")
+
+if __name__ == "__main__":
+    main()    # Esto hace que si queremos importar el código en otro programa omita la función main
 
 
 
