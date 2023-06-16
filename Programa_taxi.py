0c8c7f07d06471005a46b960d7e9a899b4a28e6a
Paloma Garcia <paloma.ggc@gmail.com>
2023-06-16 13:49:02 +0200

    arreglados probelams con la librería keyboard y prints de las teclas

--- Programa_taxi.py
+++ Programa_taxi.py
@@ -15,7 +15,7 @@ class Taxi:
         self.total = None
         self.taximetro_iniciado = False
 
-    # Definimos el método que arranca el taximetro.    
+    # Definimos el método que arranca el taximetro.
     def iniciar_taximetro(self):
         self.taximetro_iniciado = True
         self.total = 0
@@ -24,7 +24,7 @@ class Taxi:
         print("La carrera ha comenzado, ¡allá vamos!")
 
 
-    # Definimos el método que calcula la tarifa acumulada cuando nos ponemos en movimiento. 
+    # Definimos el método que calcula la tarifa acumulada cuando nos ponemos en movimiento.
     def continuar_carrera(self):
         if not self.en_movimiento:
             tiempo_arrancar = time.time()
@@ -43,7 +43,7 @@ class Taxi:
             tiempo_transcurrido = tiempo_pausa - self.tiempo_ultimo_cambio
             self.tiempo_ultimo_cambio = tiempo_pausa
             self.en_movimiento = False
-            self.total += tiempo_transcurrido * self.tarifa_movimiento 
+            self.total += tiempo_transcurrido * self.tarifa_movimiento
             euros = self.total /100
             print(f"El taxi se ha detenido temporalmente. Total acumulado {euros:.2f} euros.")
 
@@ -55,7 +55,7 @@ class Taxi:
             tiempo_transcurrido = tiempo_pausa - self.tiempo_ultimo_cambio
             self.tiempo_ultimo_cambio = tiempo_pausa
             self.en_movimiento = False
-            self.total += tiempo_transcurrido * self.tarifa_movimiento 
+            self.total += tiempo_transcurrido * self.tarifa_movimiento
 
         if not self.en_movimiento:
             tiempo_arrancar = time.time()
@@ -64,27 +64,32 @@ class Taxi:
             self.en_movimiento = False
             self.total += tiempo_transcurrido * self.tarifa_parado
 
-        euros =self.total/ 100       
+        euros =self.total/ 100
         print(f"La carrera ha terminado. Total a pagar {euros:.2f} euros.")
-    
+        self.carrera_iniciada = False
+
     # Definimos el método que muestran las opciones cuando has iniciado la carrera.
     def mostrar_opciones(self):
         if self.taximetro_iniciado:
             if self.en_movimiento:
                 print("Opciones disponibles: ")
-                print("Escribe 'p' cuando pares el taxi")
-                print("Escribe 't' para terminar la carrera y obtener el monto total")
+                print("- Pulsa 'down' cuando pares temporalmente el taxi.")
+                print("- Pulsa 'space' para terminar la carrera y obtener el total a pagar.")
+                print("------------------------------------------------------------------------")
             else:
                 print("Opciones disponibles: ")
-                print("Escribe 'c' cuando continues el recorrido del taxi")
-                print("Escribe 't' para terminar la carrera y obtener el monto total")
+                print("- Pulsa 'up' cuando continúes la marcha del taxi.")
+                print("- Pulsa 'space' para terminar la carrera y obtener el total a pagar.")
+                print("------------------------------------------------------------------------")
         else:
             print("Instrucciones de uso: ")
-            print("Escribe 'i' para iniciar una nueva carrera")
-            print("Escribe 's' para salir del programa")
-        
+            print("- Pulsa 'enter' para iniciar una nueva carrera.")
+            print("- Pulsa 'escape' para salir del programa.")
+            print("------------------------------------------------------------------------")
+
 # Definimos la función principal del programa que llamará a la clase y los métodos de taxi.
 def main():
+
     taxi = Taxi()
 
     def presionando_enter(event):
@@ -120,17 +125,19 @@ def main():
 
     print("Bienvenido a DigiTaxi")
     print("El programa para calcular las tarifas de tus carreras")
+    print(f"La tarifa en parado es de {taxi.tarifa_parado} céntimos por segundo.")
+    print(f"La tarifa en movimiento es de {taxi.tarifa_movimiento} céntimos por segundo.")
 
-    taxi.mostrar_opciones
+    taxi.mostrar_opciones()
 
-    keyboard.on_press_key("enter", presionando_enter)
-    keyboard.on_press_key("up", presionando_up)
-    keyboard.on_press_key("down ", presionando_down)
-    keyboard.on_press_key("space", presionando_space)
+    keyboard.on_press_key('enter', presionando_enter)
+    keyboard.on_press_key('up', presionando_up)
+    keyboard.on_press_key('down', presionando_down)
+    keyboard.on_press_key('space', presionando_space)
 
-    keyboard.wait("esc")
+    keyboard.wait('esc')
     keyboard.unhook_all()
-    
+
 if __name__ == "__main__":
     main()    # Esto hace que si queremos importar el código en otro programa omita la función main
 
